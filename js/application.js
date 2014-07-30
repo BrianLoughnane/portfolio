2c2afb9d98493cdb8a4b344182787a28a3059170
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-07-30 11:10:36 -0400

    js color play

--- js/application.js
+++ js/application.js
@@ -3,49 +3,49 @@
 $(document).ready(function() {
 
 
- $("header, footer, section>div").on("click", function() {
+ $("header, footer div").on("click", function() {
 
-    if($("header, footer, section>div").hasClass("red-lg")) {
+    if($(this).hasClass("red-lg")) {
         $(this).removeClass("red-lg");
         $(this).addClass("tan-lg");
     }
-    else if($("header, footer, section>div").hasClass("blue-lg")) {
+    else if($(this).hasClass("tan-lg")) {
+        $(this).removeClass("tan-lg");
+        $(this).addClass("green-lg");
+    }
+    else if($(this).hasClass("blue-lg")) {
         $(this).removeClass("blue-lg");
         $(this).addClass("red-lg");
  
     } 
-    else if($("header, footer, section>div").hasClass("tan-lg")) {
-        $(this).removeClass("tan-lg");
-        $(this).addClass("green-lg");
-    }
-    else if($("header, footer, section>div").hasClass("green-lg")) {
+    else if($(this).hasClass("green-lg")) {
         $(this).removeClass("green-lg");
         $(this).addClass("blue-lg");
     }
     
     });  
 
+ $("section div").on("click", function() {
+
+    if($(this).hasClass("red-lg")) {
+        $(this).removeClass("red-lg");
+        $(this).addClass("tan-lg");
+    }
+    else if($(this).hasClass("tan-lg")) {
+        $(this).removeClass("tan-lg");
+        $(this).addClass("green-lg");
+    }
+    else if($(this).hasClass("blue-lg")) {
+        $(this).removeClass("blue-lg");
+        $(this).addClass("red-lg");
  
- // $("header").on("click", function() {
- //    if($("header").hasClass("red-lg")) {
- //        $(this).removeClass("red-lg");
- //        $(this).addClass("tan-lg");
- //    }
- //    else if($("header").hasClass("blue-lg")) {
- //        $(this).removeClass("blue-lg");
- //        $(this).addClass("red-lg");
- 
- //    } 
- //    else if($("header").hasClass("green-lg")) {
- //        $(this).removeClass("green-lg");
- //        $(this).addClass("blue-lg");
- //    }
- //    else if($("header").hasClass("tan-lg")) {
- //        $(this).removeClass("tan-lg");
- //        $(this).addClass("green-lg");
- //    }
- 
- //    });  
+    } 
+    else if($(this).hasClass("green-lg")) {
+        $(this).removeClass("green-lg");
+        $(this).addClass("blue-lg");
+    }
+
+    }); 
 
    
 });
