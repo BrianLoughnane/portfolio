dacae44083f43697ca63fd24681e57a5ace12479
BrianLoughnane <BrianJLoughnane@gmail.com>
2014-07-30 09:39:35 -0400

    Revert "revert to prior"
    This reverts commit b37d50472666a3316aa3184646640a03212b8b0a.

--- js/application.js
+++ js/application.js
@@ -1,51 +1,77 @@
+// $(document).ready(function() {
 
+// 	$(alert("hello world"));
+
+// 	$("header").on("click", function() {
+// 		if($("header").hasClass("red-lg")) {
+// 			$(this).removeClass("red-lg");
+// 			$(this).addClass("tan-lg");
+// 		};	
+// 	});	
+
+	
+// 	$("header").on("click", function() {
+// 		if($("header").hasClass("blue-lg")) {
+// 			$(this).removeClass("blue-lg");
+// 			$(this).addClass("red-lg");
+// 		};	
+// 	});
+
+// 	if($("header").hasClass("green-lg")) {
+// 		$("header").on("click", function() {
+// 			$(this).removeClass("green-lg");
+// 			$(this).addClass("blue-lg");
+// 		});
+// 	}; 
+
+	
+// });
+
+
+
+// $(document).ready(function() {
+//  $("header").on("click", function() {
+//     if($("header").hasClass("red-lg")) {
+//         $(this).removeClass("red-lg");
+//         $(this).addClass("tan-lg");
+//     }
+//     else if($("header").hasClass("blue-lg")) {
+//         $(this).removeClass("blue-lg");
+//         $(this).addClass("red-lg");
+ 
+//     } 
+//     else if($("header").hasClass("green-lg")) {
+//         $(this).removeClass("green-lg");
+//         $(this).addClass("blue-lg");
+//     }
+//     }); 
+//  console.log("hello world");
+   
+// });
 
 $(document).ready(function() {
-
-
- $("header, footer, section>div").on("click", function() {
-
-    if($("header, footer, section>div").hasClass("red-lg")) {
+ $("header").on("click", function() {
+    if($("header").hasClass("red-lg")) {
         $(this).removeClass("red-lg");
         $(this).addClass("tan-lg");
     }
-    else if($("header, footer, section>div").hasClass("blue-lg")) {
+    else if($("header").hasClass("blue-lg")) {
         $(this).removeClass("blue-lg");
         $(this).addClass("red-lg");
  
     } 
-    else if($("header, footer, section>div").hasClass("tan-lg")) {
-        $(this).removeClass("tan-lg");
-        $(this).addClass("green-lg");
-    }
-    else if($("header, footer, section>div").hasClass("green-lg")) {
+    else if($("header").hasClass("green-lg")) {
         $(this).removeClass("green-lg");
         $(this).addClass("blue-lg");
     }
-    
-    });  
-
+    else if($("header").hasClass("tan-lg")) {
+        $(this).removeClass("tan-lg");
+        $(this).addClass("green-lg");
+    }
  
- // $("header").on("click", function() {
- //    if($("header").hasClass("red-lg")) {
- //        $(this).removeClass("red-lg");
- //        $(this).addClass("tan-lg");
- //    }
- //    else if($("header").hasClass("blue-lg")) {
- //        $(this).removeClass("blue-lg");
- //        $(this).addClass("red-lg");
+    }); 
  
- //    } 
- //    else if($("header").hasClass("green-lg")) {
- //        $(this).removeClass("green-lg");
- //        $(this).addClass("blue-lg");
- //    }
- //    else if($("header").hasClass("tan-lg")) {
- //        $(this).removeClass("tan-lg");
- //        $(this).addClass("green-lg");
- //    }
  
- //    });  
-
+ console.log("hello world");
    
 });
